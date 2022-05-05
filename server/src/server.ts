ef36f8fb6c1eafe983c710145f24542847701896
oLemos <lemmos@outlook.com.br>
2022-05-05 12:59:04 -0300

    connect email sending

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,20 @@
 import express from 'express';
+import nodemailer from 'nodemailer';
+
 import { prisma } from './prisma';
 
 const app = express();
 
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    auth: {
+        user: '21e5e70e746c02',
+        pass: 'c61c8258928faf',
+    },
+});
 
 app.post('/feedbacks', async (req, res) => {
     const { type, comment, screenshot } = req.body;
@@ -14,6 +25,18 @@ app.post('/feedbacks', async (req, res) => {
             comment,
             screenshot,
         },
+    });
+
+    transport.sendMail({
+        from: 'Equipe Feedget <oi@feedget.com>',
+        to: 'Gabriel Lemos <galemos25@gmail.com>',
+        subject: 'Novo feedback',
+        html: [
+            `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+            `<p>Tipo do feedback: ${type}</p>`,
+            `<p>Comentário do feedback: ${comment}</p>`,
+            `</div>`,
+        ].join('\n'),
     });
 
     return res.status(201).json({ data: feedback });
